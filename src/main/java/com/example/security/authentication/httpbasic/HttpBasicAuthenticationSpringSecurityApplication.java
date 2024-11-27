7cfcdc4c1957189afa722eed27a911e2db42d1de
DARYA <89320281+darishenko@users.noreply.github.com>
2024-11-27 14:43:36 +0300

    feat: implement Http Basic Authentication

---
+++ src/main/java/com/example/security/authentication/httpbasic/HttpBasicAuthenticationSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.authentication.httpbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpBasicAuthenticationSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpBasicAuthenticationSpringSecurityApplication.class, args);
+	}
+
+}
